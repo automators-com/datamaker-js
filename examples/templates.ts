7bf1d59aa381e0adbae2b7bb1b7b4c4b3b31789e
Gwinyai <gtchax@users.noreply.github.com>
2025-08-08 10:14:14 +0200

    Added fetch template by ID example

--- examples/templates.ts
+++ examples/templates.ts
@@ -2,9 +2,26 @@ import { DataMaker } from "../src/index";
 
 const datamaker = new DataMaker({});
 
+// ----------------------
+// Fetch all templates
+// ----------------------
 const fetchTemplates = async () => {
   const templates = await datamaker.getTemplates();
   console.log("All Templates:", templates);
 };
 
 fetchTemplates();
+
+// ----------------------
+// Fetch existing template by ID
+// ----------------------
+const fetchTemplateById = async () => {
+  const templateID = "template_12345";
+  const template = datamaker.getTemplateById(templateID);
+
+  console.log("Template by ID:", template);
+};
+
+fetchTemplateById()
+
+
